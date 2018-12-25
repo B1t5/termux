8dd7f69a5d2ba9aa29fc458c3d478f49a4bb8b6d
root <root@localhost.localdomain>
2018-12-25 14:51:08 -0300

    Version 0.0.1

--- instalar_todo.sh
+++ instalar_todo.sh
@@ -70,7 +70,7 @@ clear
 #Bash.bash
 mkdir .Bash.bashrc_original
 mv /data/data/com.termux/files/usr/etc/motd /data/data/com.termux/files/usr/etc/bash.bashrc /$HOME/termux/.Bash.bashrc_original
-mv bash.bashrc /data/data/com.termux/files/usr/etc
+mv /$HOME/termux/bash.bashrc /data/data/com.termux/files/usr/etc
 
 #---------------- herramientas ----------------#
 
